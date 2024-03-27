0e7a45baa51aa47e65c22fa72adffad94ff2276b
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2024-03-27 19:07:58 -0400

    Autos working

--- src/main/java/frc/team7520/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/team7520/robot/subsystems/swerve/SwerveSubsystem.java
@@ -93,7 +93,7 @@ public class SwerveSubsystem extends SubsystemBase {
                 this::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
                 this::setChassisSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
                 new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                        new PIDConstants(5.0, 0.0, 0.0),
+                        new PIDConstants(3.75, 0.0, 0.0),
                         // Translation PID constants
                         new PIDConstants(swerveDrive.swerveController.config.headingPIDF.p,
                                 swerveDrive.swerveController.config.headingPIDF.i,
